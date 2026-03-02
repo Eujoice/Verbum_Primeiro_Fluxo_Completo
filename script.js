25bbc209c257a858fedfc0ae75f74ecef28460ad
Eujoice <you@example.com>
2026-03-02 09:21:15 -0300

    JS para botão "Entrar" na tela de login

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Botão Entrar (Tela de Login)
+document.getElementById("btn-login").addEventListener("click", function() {
+    window.location.href = "acervo.html";
+});
+
 // DADOS USUÁRIO FICTICIO
 let nome = "João Bento";
 let email = "joao@email.com";
