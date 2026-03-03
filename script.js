60fcc9cd3826582d093aba39a0bb79c96ab9d28f
erickssantos <erickmiguelbsantos@gmail.com>
2026-03-03 15:53:37 -0300

    BOTOES FUNCIONANDO!!! Perfil->Acervo->Perfil->Sair

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 // Botão Entrar (Tela de Login)
-document.getElementById("btn-login").addEventListener("click", function() {
-    window.location.href = "acervo.html";
-});
+
+let btnlogin = document.getElementById("btn-login");
+
+if (btnlogin) {
+    btnlogin.addEventListener("click", function() {
+        window.location.href = "acervo.html";
+    });
+}
 
 // DADOS USUÁRIO FICTICIO
 let nome = "João Bento";
 let email = "joao@email.com";
 
-document.getElementById("nomeUsuario").textContent = nome;
-document.getElementById("emailUsuario").textContent = email;
+let nomeUsuario = document.getElementById("nomeUsuario");
+let emailUsuario = document.getElementById("emailUsuario");
+
+if (nomeUsuario && emailUsuario) {
+    nomeUsuario.textContent = nome;
+    emailUsuario.textContent = email;
+}
 
 // Botão Sair (Tela de Perfil)-> LOGOFF
-document.getElementById("btnSair").addEventListener("click", function() {
-    window.location.href = "index.html";
-});
+
+let btnSair = document.getElementById("btnSair");
+
+if (btnSair) {
+
+    btnSair.addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+}
+
+let imgUsuario = document.getElementById("iconeUsuario");
+
+if (imgUsuario) {
+
+    imgUsuario.addEventListener("click", function() {
+        window.location.href ="perfil.html";
+    });
+}
