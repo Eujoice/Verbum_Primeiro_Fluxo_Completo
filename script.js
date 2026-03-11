74ae7001c37d038c77a6972ea5158f210a9b52fa
Eujoice <you@example.com>
2026-03-11 10:16:36 -0300

    Botão "Leia mais/menos" adicionado e dinâmico, livro da tela acervo linkado com detalheslivro

--- script.js
+++ script.js
@@ -39,3 +39,29 @@ if (imgUsuario) {
         window.location.href ="perfil.html";
     });
 }
+
+// Função LEIA MAIS para a tela detalheslivro
+function leiaMais() {
+    var pontos = document.getElementById("pontos");
+    var maisTxt = document.getElementById("mais");
+    var btnLerMais = document.getElementById("btnLerMais");
+
+    if (pontos.style.display === "none") {
+        pontos.style.display = "inline";
+        maisTxt.style.display = "none";
+        btnLerMais.innerHTML = "Leia mais";
+    } else {
+        pontos.style.display = "none";
+        maisTxt.style.display = "inline";
+        btnLerMais.innerHTML = "Leia menos";
+    }
+}
+
+// Livro clicável: Acervo --> Detalhes
+let livroAcervo = document.getElementById("meiaNoiteAcervo");
+
+if (livroAcervo) {
+    livroAcervo.addEventListener("click", function() {
+        window.location.href = "detalheslivro.html";
+    }) 
+}
